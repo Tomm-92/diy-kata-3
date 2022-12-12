cd56599c56b74fbf0ea2e6959de51330bb996432
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2022-12-12 20:44:21 +0000

    json file and readme updated for project start

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -14,7 +14,7 @@ module.exports = joinNames;
 /* CODE EXPLANATION 
 
 Starting with an array of objects and want to concatenate the names together in to a string 
-First function returns an array of the names - orginally also had a split here which returned
+First function returns an array of the names - orgigit nally also had a split here which returned
 each name within their own array but found this was unecessary
 The String(names) function is required here as you cannot do a split on an array - must be a string 
 The split join returns a string which now has a space after each comma 
